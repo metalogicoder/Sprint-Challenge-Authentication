4f8b667b86791b118b9865245666f7b53301ebd0
Devon Bacon <devon.bacon@gmail.com>
2018-05-18 09:18:25 -0700

    login.js - edited callback of checkPassowrd to catch password fail

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -15,16 +15,14 @@ const login = (req, res) => {
     }
     user.checkPassword(password, (nonMatch, hashMatch) => {
       // This is an example of using our User.method from our model.
-      if (nonMatch !== null) {
-        res.status(422).json({ error: 'passwords dont match' });
-        return;
-      }
       if (hashMatch) {
         const payload = {
           username: user.username
         }; // what will determine our payload.
         const token = jwt.sign(payload, mysecret); // creates our JWT with a secret and a payload and a hash.
         res.json({ token }); // sends the token back to the client
+      } else {
+        res.status(422).json({ error: 'passwords dont match' });
       }
     });
   });
